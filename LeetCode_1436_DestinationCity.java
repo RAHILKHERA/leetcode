ee4c13dda20765ceb691bc37c23cb8bec4b240f0
RAHIL KHERA <rahilakhera@gmail.com>
2023-12-15 21:30:45 +0530

    LeetCode 1436

---
+++ LeetCode_1436_DestinationCity.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LeetCode_1436_DestinationCity {
+    
+     public String destCity(List<List<String>> paths) {
+
+            Map<String, String> map = new HashMap<>();
+
+            for (List<String> path : paths) {
+                map.put(path.get(0), path.get(1));
+            }
+
+            String destination = paths.get(0).get(0);
+            
+            while (map.containsKey(destination)) {
+                destination = map.get(destination);
+            }
+
+        return destination;
+    }
+}
