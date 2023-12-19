121f5891f8b241d93a99164a4a54264147cb7aa2
RAHIL KHERA <rahilakhera@gmail.com>
2023-12-19 15:34:03 +0530

    LeetCode 1160.

---
+++ LeetCode_1160_FindWordsThatCanBeFormedByCharacters.java
@@ -0,0 +1,43 @@
+
+/**
+ * @author Rahil Khera
+ * @email rahilakhera@gmail.com
+ * @create date 19-Dec-2023 15:02:06
+ */
+
+public class LeetCode_1160_FindWordsThatCanBeFormedByCharacters {
+    public static void main(String[] args) {
+
+        new LeetCode_1160_FindWordsThatCanBeFormedByCharacters()
+                .countCharacters(new String[] { "cat", "bt", "hat", "tree" }, "atach");
+
+    }
+
+    public int countCharacters(String[] words, String chars) {
+        int[] counts = new int[26];
+        // Step 1: Initialize Character Counts Array
+        for (int i = 0; i < chars.length(); i++) {
+            counts[chars.charAt(i) - 'a']++;
+        }
+        int res = 0;
+        // Step 3: Check Words
+        for (String s : words) {
+            if (canForm(s, counts))
+                // Step 4: Calculate Lengths
+                res += s.length();
+        }
+        return res;
+    }
+
+    boolean canForm(String word, int[] counts) {
+        int[] c = new int[26];
+        // Step 2: Update Counts Array
+        for (int i = 0; i < word.length(); i++) {
+            int x = word.charAt(i) - 'a';
+            c[x]++;
+            if (c[x] > counts[x])
+                return false;
+        }
+        return true;
+    }
+}
