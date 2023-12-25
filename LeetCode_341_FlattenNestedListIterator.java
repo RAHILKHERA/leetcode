30f054e5c170112e311f7c5ba0edf66f0553465a
RAHIL KHERA <rahilakhera@gmail.com>
2023-12-26 01:22:28 +0530

    LeetCode 341, Modified.

--- LeetCode_341_FlattenNestedListIterator.java
+++ LeetCode_341_FlattenNestedListIterator.java
@@ -1,48 +1,23 @@
 import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
+
 public class LeetCode_341_FlattenNestedListIterator {
-    
-}
-
-class Solution_LeetCode_341_FlattenNestedListIterator  {
-
-
 
 }
 
-class NestedIntegerImpl implements NestedInteger {
-
-    @Override
-    public boolean isInteger() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isInteger'");
-    }
-
-    @Override
-    public Integer getInteger() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getInteger'");
-    }
-
-    @Override
-    public List<NestedInteger> getList() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getList'");
-    }
+class Solution_LeetCode_341_FlattenNestedListIterator {
 
 }
 
 class NestedIterator implements Iterator<Integer> {
 
-   
-
     public NestedIterator(List<NestedInteger> nestedList) {
-        
+
         flattenNestedList(nestedList);
     }
 
-    private List<Integer> flattenNestedList (List<NestedInteger> nestedList) {
+    private List<Integer> flattenNestedList(List<NestedInteger> nestedList) {
 
         List<Integer> result = new ArrayList<>();
         for (NestedInteger obj : nestedList) {
@@ -53,14 +28,13 @@ class NestedIterator implements Iterator<Integer> {
             }
         }
 
-         return result;
+        return result;
 
     }
 
-
     @Override
     public Integer next() {
-        return null;      
+        return null;
     }
 
     @Override
@@ -69,20 +43,21 @@ class NestedIterator implements Iterator<Integer> {
     }
 }
 
-
- // This is the interface that allows for creating nested lists.
- // You should not implement it, or speculate about its implementation
+// This is the interface that allows for creating nested lists.
+// You should not implement it, or speculate about its implementation
 interface NestedInteger {
- 
-      // @return true if this NestedInteger holds a single integer, rather than a nested list.
-      public boolean isInteger();
- 
-      // @return the single integer that this NestedInteger holds, if it holds a single integer
-      // Return null if this NestedInteger holds a nested list
-      public Integer getInteger();
- 
-      // @return the nested list that this NestedInteger holds, if it holds a nested list
-      // Return empty list if this NestedInteger holds a single integer
-      public List<NestedInteger> getList();
- }
- 
+
+    // @return true if this NestedInteger holds a single integer, rather than a
+    // nested list.
+    public boolean isInteger();
+
+    // @return the single integer that this NestedInteger holds, if it holds a
+    // single integer
+    // Return null if this NestedInteger holds a nested list
+    public Integer getInteger();
+
+    // @return the nested list that this NestedInteger holds, if it holds a nested
+    // list
+    // Return empty list if this NestedInteger holds a single integer
+    public List<NestedInteger> getList();
+}
