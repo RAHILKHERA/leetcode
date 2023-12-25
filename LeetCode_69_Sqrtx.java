513a259f9ac4fd726f63e57aa9f2a47bc9d4f09d
RAHIL KHERA <rahilakhera@gmail.com>
2023-12-26 01:17:14 +0530

    LeetCode 69, Removed unwanted imports.

--- LeetCode_69_Sqrtx.java
+++ LeetCode_69_Sqrtx.java
@@ -1,5 +1,3 @@
-import java.util.HashMap;
-
 public class LeetCode_69_Sqrtx {
     public static void main(String[] args) {
         new Solution_LeetCode_69_Sqrtx().mySqrt(2147483647);
@@ -7,24 +5,20 @@ public class LeetCode_69_Sqrtx {
 }
 
 class Solution_LeetCode_69_Sqrtx {
-    
-    private static HashMap<Long, Integer> map = new HashMap<Long, Integer>();
 
-    
-    
     public int mySqrt(int x) {
-        
+
         if (x == 0) {
             return 0;
         }
-        
+
         long left = 1;
         long right = x / 2 + 1;
-        
+
         while (left <= right) {
             long mid = left + (right - left) / 2;
             long square = mid * mid;
-            
+
             if (square == x) {
                 return (int) mid;
             } else if (square < x) {
@@ -33,7 +27,7 @@ class Solution_LeetCode_69_Sqrtx {
                 right = mid - 1;
             }
         }
-        
+
         return (int) right;
     }
 }
