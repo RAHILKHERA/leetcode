cb02e558d3b45107f3a03383cb512a438af4f239
RAHIL KHERA <rahilakhera@gmail.com>
2024-01-06 23:52:11 +0530

    LeetCode 332, Modified implementation to resolve TLE.

--- LeetCode_332_ReconstructItinerary.java
+++ LeetCode_332_ReconstructItinerary.java
@@ -6,102 +6,72 @@ import java.util.Arrays;
 
 public class LeetCode_332_ReconstructItinerary {
     public static void main(String[] args) {
-        List<List<String>> tickets  =  new ArrayList<List<String>>() ;
-        // List<String> ticket0 = Arrays.asList("JFK", "SFO");
-        // List<String> ticket1 = Arrays.asList("JFK", "ATL");
-        // List<String> ticket2 = Arrays.asList("SFO", "ATL");
-        // List<String> ticket3 = Arrays.asList("ATL", "JFK");
-        // List<String> ticket4 = Arrays.asList("ATL", "SFO");
-
-        // tickets.add(ticket0);
-        // tickets.add(ticket1);
-        // tickets.add(ticket2);
-        // tickets.add(ticket3);
-        // tickets.add(ticket4);
-           
-        
-        // List<String> ticket0 = Arrays.asList("MUC", "LHR");
-        // List<String> ticket1 = Arrays.asList("JFK", "MUC");
-        // List<String> ticket2 = Arrays.asList("SFO", "SJC");
-        // List<String> ticket3 = Arrays.asList("LHR", "SFO");
-        
-
-        // tickets.add(ticket0);
-        // tickets.add(ticket1);
-        // tickets.add(ticket2);
-        // tickets.add(ticket3);
-
-        List<String> ticket0 = Arrays.asList("JFK", "KUL");
-        List<String> ticket1 = Arrays.asList("JFK", "NRT");
-        List<String> ticket2 = Arrays.asList("NRT", "JFK");
-
-        
+        List<List<String>> tickets = new ArrayList<List<String>>();
+        List<String> ticket0 = Arrays.asList("JFK", "SFO");
+        List<String> ticket1 = Arrays.asList("JFK", "ATL");
+        List<String> ticket2 = Arrays.asList("SFO", "ATL");
+        List<String> ticket3 = Arrays.asList("ATL", "JFK");
+        List<String> ticket4 = Arrays.asList("ATL", "SFO");
 
         tickets.add(ticket0);
         tickets.add(ticket1);
         tickets.add(ticket2);
+        tickets.add(ticket3);
+        tickets.add(ticket4);
 
+        // List<String> ticket0 = Arrays.asList("MUC", "LHR");
+        // List<String> ticket1 = Arrays.asList("JFK", "MUC");
+        // List<String> ticket2 = Arrays.asList("SFO", "SJC");
+        // List<String> ticket3 = Arrays.asList("LHR", "SFO");
+
+        // tickets.add(ticket0);
+        // tickets.add(ticket1);
+        // tickets.add(ticket2);
+        // tickets.add(ticket3);
+
+        // List<String> ticket0 = Arrays.asList("JFK", "KUL");
+        // List<String> ticket1 = Arrays.asList("JFK", "NRT");
+        // List<String> ticket2 = Arrays.asList("NRT", "JFK");
+
+        // tickets.add(ticket0);
+        // tickets.add(ticket1);
+        // tickets.add(ticket2);
 
         new Solution_LeetCode_332_ReconstructItinerary().findItinerary(tickets);
     }
 }
 
-class  Solution_LeetCode_332_ReconstructItinerary {
+class Solution_LeetCode_332_ReconstructItinerary {
     public List<String> findItinerary(List<List<String>> tickets) {
-        
-        HashMap<String, List<String>> destinationMap  = new HashMap<String, List<String>>();
 
-        for ( List<String> ticket : tickets) {
+        HashMap<String, List<String>> destinationMap = new HashMap<String, List<String>>();
 
-            String departure = ticket.get(0);
-            String arrival = ticket.get(1);
-            if (destinationMap.containsKey(departure)) {
-                List<String> destinations = destinationMap.get(departure);
-                destinations.add(arrival);
-
-            } else {
-                List<String> destinations = new ArrayList<String>(); 
-                destinations.add(arrival);
-                destinationMap.put(departure, destinations);
-            }
+        for (List<String> ticket : tickets) {
+            destinationMap.computeIfAbsent(ticket.get(0), key -> new ArrayList<>())
+                    .add(ticket.get(1));
         }
 
         for (Map.Entry<String, List<String>> entry : destinationMap.entrySet()) {
-            entry.getValue().sort((a,b) -> { 
-                if (destinationMap.containsKey(a) && destinationMap.containsKey(b)) {
-                    return a.compareTo(b) ;
-                } else {
-                    if(destinationMap.containsKey(a)) {
-                        return -1;
-                    } else {
-                        return 1;
-                    }
-                }
-                
-            });
+            entry.getValue().sort(String::compareTo);
         }
 
         List<String> itinerary = new ArrayList<String>();
-        String nextStop = "JFK";
-        String currentStop = "";
-        itinerary.add(nextStop);
-        while (destinationMap.size() > 0) {
-            
-            List<String> availableDestinations = destinationMap.get(nextStop);
-            if (availableDestinations != null) {
-                currentStop = nextStop;
-                nextStop = availableDestinations.get(0);
-            }
-          
-            itinerary.add(nextStop);
-            if (availableDestinations != null) {
-                availableDestinations.remove(0);
-                if (availableDestinations.size() == 0) {
-                    destinationMap.remove(currentStop);
-                }
-            }
-            
-        }        
+        dfs("JFK", destinationMap, itinerary);
         return itinerary;
+    }
+
+    private void dfs(String source, Map<String, List<String>> destinationMap, List<String> itinerary) {
+
+        List<String> neighbors = destinationMap.get(source);
+
+        if (neighbors != null) {
+            while (!neighbors.isEmpty()) {
+                String nextDestination = neighbors.remove(0);
+                dfs(nextDestination, destinationMap, itinerary);
+            }
+        }
+
+        // Add the current source to the beginning of the itinerary
+        itinerary.add(0, source);
     }
 }
