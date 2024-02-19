6f9c5258a826ec9b320a21f89095e7969393ae2b
RAHIL KHERA <rahilakhera@gmail.com>
2024-02-19 22:50:24 +0530

    ListNode, Added toString method.

--- ListNode.java
+++ ListNode.java
@@ -1,7 +1,27 @@
 public class ListNode {
     int val;
     ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+
+        if (next != null) {
+            return "{ value : " + val + ", next : " + next.toString() + "}";
+        }
+
+        return "{ value : " + val + ", next : null}";
+    }
+
 }
