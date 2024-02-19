b0183918fb0384c00966df1cadd07eebfc46d50e
RAHIL KHERA <rahilakhera@gmail.com>
2024-02-19 19:54:47 +0530

    Behavioral Pattern, Chain Of Responsibility.

---
+++ DesignPattern/BehavioralPatterns/ChainOfResponsibility/ChainPatternDemo.java
@@ -0,0 +1,31 @@
+package DesignPattern.BehavioralPatterns.ChainOfResponsibility;
+
+public class ChainPatternDemo {
+    private static AbstractLogger getChainOfLoggers() {
+
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+        return errorLogger;
+    }
+
+    public static void main(String[] args) {
+        AbstractLogger loggerChain = getChainOfLoggers();
+
+        loggerChain.logMessage(AbstractLogger.INFO,
+                "This is an information.");
+
+        loggerChain.logMessage(AbstractLogger.DEBUG,
+                "This is an debug level information.");
+
+        loggerChain.logMessage(AbstractLogger.ERROR,
+                "This is an error information.");
+
+        loggerChain.logMessage(10,
+                "This is an invalid level information.");
+    }
+}
---
+++ DesignPattern/BehavioralPatterns/ChainOfResponsibility/ErrorLogger.java
@@ -0,0 +1,13 @@
+package DesignPattern.BehavioralPatterns.ChainOfResponsibility;
+
+public class ErrorLogger extends AbstractLogger {
+
+    public ErrorLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Error Console::Logger: " + message);
+    }
+}
